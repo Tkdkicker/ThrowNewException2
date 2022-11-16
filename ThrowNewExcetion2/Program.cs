54b2c85468432f52d205b1303078e9b2da5dfdbd
gemma <gemma@DESKTOP-2Q2G917>
2022-11-16 10:57:57 +0000

    Add project files.

---
+++ ThrowNewExcetion2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ThrowNewExcetion2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string empty = Name;
+
+            IFormatProvider provider;
+            DateTime dateTime = ToDateTime(provider);
+        }
+
+        /// <summary>
+        /// Null coalescing operator:
+        /// A throw expression is used to throw an exception if the string assigned to a Name property is null
+        /// </summary>
+        public static string Name
+        {
+            get => name;
+            set => name = value ??
+                throw new ArgumentNullException(paramName: nameof(value), message: "Name cannot be null");
+        }
+
+        /// <summary>
+        /// Expression-bodied lambda/method:
+        /// Throws an InvalidCastException because a conversion to a DateTime value is not supported
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        private static DateTime ToDateTime(IFormatProvider provider) =>
+         throw new InvalidCastException("Conversion to a DateTime is not supported.");
+    }
+}
